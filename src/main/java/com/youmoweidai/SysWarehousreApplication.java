5df11b0784ccae4483cc944db4e64ff0fa5eb664
youmoweidai <1914054441@qq.com>
2021-01-05 10:19:29 +0800

    Initial commit

---
+++ src/main/java/com/youmoweidai/SysWarehousreApplication.java
@@ -0,0 +1,13 @@
+package com.youmoweidai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysWarehousreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SysWarehousreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/youmoweidai/SysWarehousreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youmoweidai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysWarehousreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
